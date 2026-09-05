cde81a7ee5cc3ad79cdff288209b69898cc9d3fa
Percie Louise Y. Samaniego <pysamaniego1@up.edu.ph>
2026-09-06 03:26:09 +0800

    meta: ignore IDE-specific files for .idea

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:isko_later/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const IskoLaterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

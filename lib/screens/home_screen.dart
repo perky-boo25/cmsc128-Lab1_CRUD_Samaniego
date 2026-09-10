934625f589fbee306ddf0ecf2dfc8a08df45f2e1
Percie Louise Y. Samaniego <pysamaniego1@up.edu.ph>
2026-09-10 22:49:12 +0800

    fix: correct greeting time condition for morning

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -65,7 +65,7 @@ class _HomeHeader extends StatelessWidget {
   String _getGreeting() {
     final hour = DateTime.now().hour;
 
-    if (hour >= 1 && hour < 12) {
+    if (hour >= 0 && hour < 12) {
       return 'Good morning, ';
     } else if (hour >= 12 && hour < 18) {
       return 'Good afternoon, ';
